c8cdd756c562a6258280d756f741626992180c0e
Chris Pearce <chris@pearce.org.nz>
2009-11-26 14:38:59 +1300

    Build validator with -O2 and witout -g.

--- build-validator.sh
+++ build-validator.sh
@@ -1,3 +1,3 @@
 SRC="src/Decoder.cpp src/OggIndexValid.cpp src/Options.cpp src/SkeletonEncoder.cpp src/Utils.cpp src/Validate.cpp"
 
-g++ -g $SRC -Wall -l ogg -l theoradec -l vorbis -o OggIndexValid
+g++ -O2 $SRC -Wall -l ogg -l theoradec -l vorbis -o OggIndexValid
